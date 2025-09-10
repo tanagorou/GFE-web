fc92e277c6cb575f0280e369dd40fa08c13ec953
souta <tanagorou20181014@gmail.com>
2025-09-10 21:08:33 +0900

    add: トークンによる認証を追加

--- app/backend/app/controllers/application_controller.rb
+++ app/backend/app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds # APIモード
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_cors_headers
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :set_cors_headers
+  # before_action :authenticate_request
+
+  # Cookieを扱う
+  include ActionController::Cookies
+  # 認可を行う
+  include UserAuthenticateService
 
   private
 
@@ -19,4 +25,26 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :password, :password_confirmation ])
   end
+
+
+#   def authenticate_request
+#     header = request.headers['Authorization']
+#     header = header.split(' ').last if header
+#     begin
+#       @decoded = JwtService.decode(header)
+#       if @decoded["provider"] == "guest"
+#         @current_user = User.find(@decoded["user_id"])
+#       else
+#         user_auth = User.find_by(uid: @decoded["google_user_id"], provider: @decoded["provider"])
+#         @current_user = user_auth.user if user_auth
+#       end
+#       Rails.logger.info(@current_user)
+#       unless @current_user
+#         raise ActiveRecord::RecordNotFound, 'User not found'
+#       end
+#     rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+#       Rails.logger.error "認証エラー: #{e.message}"
+#       render json: { errors: e.message }, status: :unauthorized
+#     end
+#   end
 end
