38b415816e2e3a8a6dc5f069d99ce3fdca1e7ca3
souta <tanagorou20181014@gmail.com>
2025-09-11 19:39:42 +0900

    fix: ルーティングの設定

--- app/frontend/src/routes/index.tsx
+++ app/frontend/src/routes/index.tsx
@@ -3,15 +3,22 @@ import SignIn from "../components/signin/SignIn";
 import SignUp from "../components/signup/SignUp";
 import Home from "../pages/Home";
 import Layout from "../layouts/Layout";
+import StudyPage from "../pages/StudyPage";
+
+
 
 export default function AppRoutes(){
   return (
-    <Routes>
-      <Route path='/signin' element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} /> 
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/signin' element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} /> 
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/study/*" element={<StudyPage/>}/>
+        </Route>
+      </Routes>
+      <StudyPage/>
+    </>
   )
 }
