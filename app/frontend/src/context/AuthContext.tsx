1f59992916b3c04bc98b707cd192382d45e47f48
souta <tanagorou20181014@gmail.com>
2025-09-13 15:22:40 +0900

    add: authTokenで状態を管理。その他、Userがログインしているかどうかの関数を追加

--- app/frontend/src/context/AuthContext.tsx
+++ app/frontend/src/context/AuthContext.tsx
@@ -1,59 +1,63 @@
 import axios from "axios"
+import axiosCaseConverter from "simple-axios-case-converter";
 import { createContext, useContext, useEffect, useState } from "react"
+import {setAccessToken, setCurrentUser, isExistUser, isExpired, isExistUserAndExpired} from "./Auth"
+
 
 type AuthContextType = {
-  isAuthenticated: boolean
-  token: string | null
+  // isAuthenticated: boolean
+  // token: string | null
   login: (token: string) => void
   logout: () => void
-  getToken: () => string | null
-  setToken: (v:string | null) => void
-  currentUser: string | null
-  setCurrentUser: (v: string | null) => void
+  // getToken: () => string | null
+  // setToken: (v:string | null) => void
+  // currentUser: string | null
+  // setCurrentUser: (v: string | null) => void
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
-  const [token, setToken] =  useState<string | null>(localStorage.getItem('token'))
-  const [currentUser, setCurrentUser] = useState<string | null>(null)
+  // const [currentUser, setCurrentUser] = useState<string | null>(null)
 
+  const [authToken, setAuthToken] = useState({user:{current: null}, auth: {token: null, expires: 0, payload: {}}})
+
+  // useEffect(() => {
+  //   const query = new URLSearchParams(window.location.search)
+  //   const tokenFromUrl = query.get('token')
+  //   if(tokenFromUrl){
+  //     setToken(tokenFromUrl)
+  //     localStorage.setItem('authToken',tokenFromUrl)
+  //   } else {
+  //     const storedToken = localStorage.getItem('authToken')
+  //     if(storedToken){
+  //       setToken(storedToken)
+  //     }
+  //   }
+  // },[])
   useEffect(() => {
-    const storedToken = localStorage.getItem('token')
-    if(storedToken){
-      setToken(storedToken)
-    }
-  },[])
-
-  useEffect(() => {
-    const query = new URLSearchParams(window.location.search)
-    const tokenFromUrl = query.get('token')
-    if(tokenFromUrl){
-      setToken(tokenFromUrl)
-      localStorage.setItem('authToken',tokenFromUrl)
-    } else {
-      const storedToken = localStorage.getItem('authToken')
-      if(storedToken){
-        setToken(storedToken)
-      }
-    }
-  },[])
+    console.log(authToken)
+  },[authToken])
 
 
-  const login = (newToken: string) => {
-    localStorage.setItem('accessToken', newToken)
-    setToken(newToken)
+  const login = (data: any) => {
+    const user = setCurrentUser(data)
+    const accessToken = setAccessToken(data)
+    const merge = {...user, ...accessToken}
+    setAuthToken({...setCurrentUser(data), ...setAccessToken(data)})
+    // console.log(user,accessToken)
+    console.log(merge)
+    console.log(authToken)
   }
 
   const logout = () => {
-    localStorage.removeItem('authToken')
-    setToken(null)
+    
   }
 
-  const getToken = () => token
+  // const getToken = () => token
   
   return (
-    <AuthContext.Provider value={{ isAuthenticated: !!token, token, login, logout, getToken, setToken, currentUser, setCurrentUser}}>
+    <AuthContext.Provider value={{ login, logout}}>
       {children}
     </AuthContext.Provider>
   )
