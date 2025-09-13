e930e890b764d331820eb25eb40dddc0f7362d95
souta <tanagorou20181014@gmail.com>
2025-09-13 16:07:45 +0900

    fix: ページ遷移でリロードが行われているのを解決

--- app/frontend/src/components/Sidebar/Sidebar.tsx
+++ app/frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
 import { SidebarData } from "./SidebarData"
 import './Sidebar.css'
+import { useNavigate } from "react-router-dom"
 
 export default function Sidebar(){
+  const navigate = useNavigate()
   return(
     <div className="Sidebar">
       <ul className="SidebarList">
         {SidebarData.map((val, key) => {
           return(
-            <li key={key} className="row" onClick={() => window.location.pathname = val.link}>
+            <li key={key} className="row" onClick={() => navigate(val.link)}>
               <div id="icon">{val.icon}</div>
               <div id="title">{val.title}</div>
             </li>
