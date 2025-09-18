7ff3986f77b2ae86d2552bdbb514c628d6373f14
souta <tanagorou20181014@gmail.com>
2025-09-18 21:25:16 +0900

    feat: ページ遷移が行われてもタイマーが作動し続けるように設定

--- app/frontend/src/components/Timer.tsx
+++ app/frontend/src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import CircularProgress from './CircularProgress'
 import { IconButton } from '@mui/material';
 import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
@@ -6,6 +6,8 @@ import RotateLeftOutlinedIcon from '@mui/icons-material/RotateLeftOutlined';
 import StopCircleOutlinedIcon from '@mui/icons-material/StopCircleOutlined';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
+import { useStudyTime } from '../context/StudyContext';
+
 
 const TimerContainer = styled('div')({
   display: 'flex',
@@ -37,92 +39,37 @@ let completedTermsStudy = 0
 let completedTermsRest = 0
 
 export default function Timer({timeData, totalTime, storeTotal}: timerProps){
-  const [ timerState, setTimerState ] = useState<string>('standby')
-  const [ timerCount, setTimerCount] = useState<number>(timeData.studyTime)
-  const [ restState, setRestState ] = useState<boolean>(false)
 
-  const timerIdRef = useRef<NodeJS.Timeout | undefined>(undefined)
+  const { studyTime, restTime, restState, setRestState,setNextTimeState, timerCount, setTimerCount} = useStudyTime()
 
-  const formatTime = (msSeconds: number) => {
-    const msSafe = Math.max(msSeconds, 0)
-    const hour = Math.floor(msSafe / ONE_HOURS);
-    const minute = Math.floor((msSafe % ONE_HOURS) / ONE_MINUTES);
-    const second = Math.floor((msSafe % ONE_MINUTES) / ONE_SECONDS);
-    return [hour, minute, second]
-      .map((val: any) => String(val).padStart(2, "0"))
-      .join(":");
-  };
-
-  const handleState = () => {
-    console.log(restState)
-    const next = !restState
-    setRestState(next)
-    if(next){
-      completedTermsStudy = completedTermsStudy + 1
-      setTimerCount(timeData.restTime)
-    } else {
-      completedTermsRest = completedTermsRest + 1
-      setTimerCount(timeData.studyTime)
-    }
-  }
 
   const start = () => {
-    if(timeData.studyTime !== 0){
-      setTimerState('active')
+    if(studyTime !== 0){
+      setNextTimeState('active')
     }
   }
 
   const stop = () => {
-    setTimerState('standby')
-    storeTotal(eachTotalTime())
+    setNextTimeState('standby')
+    // storeTotal(eachTotalTime())
   }
 
   const reset = () => {
-    setTimerState('standby')
-    setRestState(restState ? false : true) //リセットときに休憩かどうかの判定が逆転してしまうので修正
-    setTimerCount(0)
-  }
-
-  //親から渡された時間をセット
-  useEffect(() => {
-    setTimerCount(timeData.studyTime)
-  },[timeData])
-
-  useEffect(() => {
-    if(timerState !== 'active'){
-      return
-    }
-    if(timerCount >= 0){
-      timerIdRef.current = setInterval(() => {
-        setTimerCount((prev) => prev - 1000)
-      }, 1000);
+    console.log('rest', restState)
+    setNextTimeState('standby')
+    // setRestState(restState ? false : true) //リセットときに休憩かどうかの判定が逆転してしまうので修正
+    if(restState){
+      setTimerCount(restTime)
     } else {
-      handleState()
+      setTimerCount(studyTime)
     }
-
-    return() => {
-      clearInterval(timerIdRef.current)
-    }
-  },[timerState, timerCount])
-
-  const eachTotalTime = () => {
-    totalTime.study = timeData.studyTime * completedTermsStudy
-    totalTime.rest = timeData.restTime * completedTermsRest
-    if(!restState){
-      totalTime.study = totalTime.study + timeData.studyTime - timerCount
-    } else {
-      totalTime.rest  = totalTime.rest + timeData.restTime - timerCount
-    }
-    return totalTime
   }
-
-  
 
   return(
     <TimerContainer>
       <TimerCircle>
         <CircularProgress 
-          totalMs={ restState ? timeData.restTime : timeData.studyTime } 
+          totalMs={ restState ? restTime : studyTime } 
           remainingMs={ timerCount }
           />
       </TimerCircle>
--- app/frontend/src/context/StudyContext.tsx
+++ app/frontend/src/context/StudyContext.tsx
@@ -1,16 +1,55 @@
 // ページ遷移が行われても、設定した勉強時間がリセットされないように
 // する
 
-import React, { createContext, ReactNode, useContext, useEffect, useState } from "react"
+import React, { createContext, ReactNode, useContext, useEffect, useState, useRef } from "react"
 
 const StudyTimeContext = createContext<any>(undefined)
 
 export const StudyTimeProvider: React.FC<{children: ReactNode}> = ({children}) => {
   const [studyTime, setStudyTime] = useState(0)
   const [restTime, setRestTime] = useState(0)
+  const [timerCount, setTimerCount] = useState(0)
+  const [nextTimeState, setNextTimeState] = useState('standby')
+  const [restState, setRestState] = useState(false)
+  const timerIdRef = useRef<NodeJS.Timeout | undefined>(undefined)
+
+  // タイマーをContext内で管理
+  // ページ遷移が行われてもタイマーが停止しないように改良
+  const handleState = () => {
+    const next = !restState
+    setRestState(next)
+    if(restState){
+      setTimerCount(studyTime)
+    }else{
+      setTimerCount(restTime)
+    }
+  }
+
+  useEffect(() => {
+    setTimerCount(studyTime)
+  },[studyTime])
+
+
+  useEffect(() => {
+    if(nextTimeState !== 'active'){
+      return
+    }
+    if(timerCount >= 0){
+      console.log('timerCount:',timerCount)
+      timerIdRef.current = setInterval(() => {
+        setTimerCount((prev) => prev - 1000)
+      }, 1000)
+    } else {
+      console.log('タイマー終了')
+      handleState()
+    }
+    return() => {
+      clearInterval(timerIdRef.current)
+    }
+  },[nextTimeState, timerCount])
 
   return (
-    <StudyTimeContext.Provider value={{studyTime, setStudyTime, restTime, setRestTime}}>
+    <StudyTimeContext.Provider value={{studyTime, setStudyTime, restTime, setRestTime, nextTimeState, setNextTimeState, timerCount, setTimerCount, restState, setRestState}}>
       {children}
     </StudyTimeContext.Provider>
   )
