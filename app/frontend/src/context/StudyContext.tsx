2549f8888d3507b914ced14448f44c990053f39a
souta <tanagorou20181014@gmail.com>
2025-09-18 22:00:43 +0900

    feat: 勉強と作業のポモドーロ回数を追加

--- app/frontend/src/context/StudyContext.tsx
+++ app/frontend/src/context/StudyContext.tsx
@@ -11,31 +11,41 @@ export const StudyTimeProvider: React.FC<{children: ReactNode}> = ({children}) =
   const [timerCount, setTimerCount] = useState(0)
   const [nextTimeState, setNextTimeState] = useState('standby')
   const [restState, setRestState] = useState(false)
+  const [pomodoreCount, setPomodoreCount] = useState({study: 0, rest: 0})
+  const [totalTime, setTotalTime] = useState({study: 0, rest: 0})
   const timerIdRef = useRef<NodeJS.Timeout | undefined>(undefined)
+  
 
-  // タイマーをContext内で管理
-  // ページ遷移が行われてもタイマーが停止しないように改良
+  // タイマーをContext内で管理、ページ遷移が行われてもタイマーが停止しないように改良
   const handleState = () => {
     const next = !restState
     setRestState(next)
-    if(restState){
-      setTimerCount(studyTime)
-    }else{
+    console.log('next:',next)
+    if(next){
       setTimerCount(restTime)
+      // 休憩時間に入ったので、勉強ポモドーロカウントを＋１増加させる
+      setPomodoreCount((prev) => ({...prev, study: prev.study + 1}))
+    }else{
+      setTimerCount(studyTime)
+      // 勉強時間に入ったので、休憩ポモドーロカウントを＋１増加させる
+      setPomodoreCount((prev) => ({...prev, rest: prev.rest + 1}))
     }
   }
 
+
+  // 初期値にstudyTimerをセット。
   useEffect(() => {
     setTimerCount(studyTime)
   },[studyTime])
 
 
   useEffect(() => {
+    // console.log('pomodoreCount:',pomodoreCount)
     if(nextTimeState !== 'active'){
       return
     }
     if(timerCount >= 0){
-      console.log('timerCount:',timerCount)
+      // console.log('timerCount:',timerCount)
       timerIdRef.current = setInterval(() => {
         setTimerCount((prev) => prev - 1000)
       }, 1000)
